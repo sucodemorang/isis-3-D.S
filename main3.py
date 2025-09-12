e494a860eb1a31c2b439139839ccaf7afee8247e
Isis rafae <161063552+sucodemorang@users.noreply.github.com>
2025-09-12 11:52:51 -0300

    Add files via upload

--- main3.py
+++ main3.py
@@ -1,36 +1,73 @@
-import pygame
+import pygame 
 import os
 
 
 pygame.init()
 
 
-WIDHT, HEIGHT = 800, 600
-screen = pygame.display.set_mode((WIDHT, HEIGHT))
-pygame.display.set_caption("Janela com Imagem")
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+pygame.display.set_caption("Mover imagem com Setas")
 
 
-BG_COLOR = (30, 30, 40)
+BG_COLOR = (193, 0, 40)
 
 
-image_file = "player.png"
-if os.path.exist(image_file):
-    img = pygame.image.load(image_file).convert_alpha()
-    img_rect = img.get_rect(center=(WIDHT // 2, HEIGHT // 2))
+image_file = "GAME\\player.png"
+if os.path.exists(image_file):
+   img = pygame.image.load(image_file).convert_alpha()
+   img_rect = img.get_rect(center=(WIDTH // 2, HEIGHT // 2))
 else:
-   print("Imagem não encontrada!")
+   print("Imagem não encontrada")
 
 
 SPEED = 1
 
 
+def centralize_image():
+   global img_rect, WIDTH, HEIGHT
+   img_rect.center = (WIDTH // 2, HEIGHT // 2)
+
+
+last_width, last_height = WIDTH, HEIGHT
+
+
 running = True
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+   for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+         running = False
+
+
+current_width, current_height = screen.get_size()
+
+
+if current_width != last_width or current_height !=last_height:
+   WIDTH, HEIGHT = current_width, current_height
+   centralize_image()
+   last_width, last_height = current_width, current_height
 
 
 keys = pygame.key.get_pressed()
 
-#paramos por aq
+
+if keys[pygame.K_LEFT]:
+   img_rect.x -= SPEED
+if keys[pygame.K_RIGHT]:
+   img_rect.x += SPEED
+if keys[pygame.K_UP]:
+   img_rect.y -= SPEED
+if keys[pygame.K_DOWN]:
+   img_rect.y +=SPEED
+
+
+screen.fill(BG_COLOR)
+
+
+screen.blit(img, img_rect.topleft)
+
+
+pygame.display.flip()
+
+
+pygame.quit
---
+++ main3move.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+
+
+if keys[pygame.K_LEFT]:
+    img_rect.x -= SPEED 
+if keys[pygame.K_RIGHT]:
+    img_rect.x -= SPEED
+    
